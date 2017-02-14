d223c732bdb940e1b0e2f83c6b11ea17d5d9fd3d
epiphanatic <simpson.jason.r@gmail.com>
2017-02-14 17:30:57 +1000

    added to README

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,81 @@
 import media
 import fresh_tomatoes
-# since media is the file that contains all the classes, it is also the 'library'
-# following calls the init function in the Movie class in the media library that requires arguments listed
-toy_story = media.Movie("Toy Story",
-                        "A Story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+# Since media is the file that contains all the classes, it is also the
+#   'library'
 
-the_matrix = media.Movie("The Matrix",
-                         "I took the red pill...",
-                         "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-                         "https://www.youtube.com/watch?v=m8e-FF8MsqU")
+# The following calls the init function in the Movie class in the media
+#   library that requires arguments listed in order to make instances
+#   of the Movie class that will be fed to the website.
+# So for example, toy_story is an instance of the Movie class in the media
+#   library. As is avatar, the_matrix, etc. Each of the lines are a
+#   parameter that becomes an instance variable in the Movie init def.
+# The init def requires 4 parameters (movie_title, movie_storyline,
+#   poster_image, trailer_youtube), hence the 4 lines in each instance
+#   that make up these parameters.
 
-fight_club = media.Movie("Fight Club",
-                         "You're not your fucking khakis",
-                         "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
-                         "https://www.youtube.com/watch?v=SUXWAEX2jlg")
+toy_story = media.Movie(
+    "Toy Story",
+    "A Story of a boy and his toys that come to life",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4"
+)
 
-shawshank_redemption = media.Movie("The Shawshank Redemption",
-                                   "Get busy livin, or get busy dyin, that's goddamned right",
-                                   "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+)
 
-three_hundred = media.Movie("300",
-                            "Does anybody know wtf this is about? seriously",
-                            "https://upload.wikimedia.org/wikipedia/en/5/5c/300poster.jpg",
-                            "https://www.youtube.com/watch?v=UrIbxk7idYA")
+the_matrix = media.Movie(
+    "The Matrix",
+    "I took the red pill...",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU"
+)
 
-movies = [toy_story, avatar, the_matrix, fight_club, shawshank_redemption, three_hundred]
+fight_club = media.Movie(
+    "Fight Club",
+    "You're not your fucking khakis",
+    "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
+    "https://www.youtube.com/watch?v=SUXWAEX2jlg"
+)
+
+shawshank_redemption = media.Movie(
+    "The Shawshank Redemption",
+    "Get busy livin, or get busy dyin, that's goddamned right",
+    "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=6hB3S9bIaco"
+)
+
+three_hundred = media.Movie(
+    "300",
+    "Does anybody know wtf this is about? seriously",
+    "https://upload.wikimedia.org/wikipedia/en/5/5c/300poster.jpg",
+    "https://www.youtube.com/watch?v=UrIbxk7idYA"
+)
+
+# The following list provides the movies that we wish to render in
+#   fresh_tomatoes.py
+# While the instances are created above, the only ones that will actually
+#   get rendered are what gets put in the list.
+# So for example, if you remove three_hundred from the list and run the
+#   script, then the movie 300 will not show up.
+
+movies = [
+    toy_story, avatar, the_matrix, fight_club, shawshank_redemption,
+    three_hundred
+]
+
+# The following calls the open_movies_page def in fresh_tomatoes.py, and
+#   passes it the list above so that it knows which movies to show.
+# By doing this, a browser window is opened and the movies are listed.
 
 fresh_tomatoes.open_movies_page(movies)
+
+# The following line will print the documentation for the Media class,
+#   which is located right beneath the class declaration in media.py
+#   in triple quotes.
+
+print (media.Movie.__doc__)
--- media.py
+++ media.py
@@ -2,17 +2,41 @@ import webbrowser
 
 
 class Movie:
-    """This is documentation, notice the triple quotes. Print __doc__ to see"""
+
+    """
+    This class allows code elsewhere to create instances of the class
+    Movie.
+
+    It has an init method that takes in four parameters:
+       movie_title, movie_storyline, poster_image, trailer_youtube
+    and assigns the value of those parameters to instance variables.
+
+    The instances created are then later passed to fresh_tomatoes.py where
+       they are used to provide information to the webpage to be displayed
+
+    The class contains one class variable (VALID_RATINGS) that can be used
+       to check whether or not a rating is valid if one chooses to include
+       that information in their webpage.
+
+    The class contains one instance method (show_trailer), that plays the
+       movie trailer for whatever instance is passed to it (self).
+
+    """
 
     # this is a class variable - can be used for all instances.
     # when declaring static variables like this, make it all caps
+
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    # Remember, init creates space in memory for a new instance of Movie when the class is called
-    # below is also known as the constructor
-    # self is the object/instance being created via the function that instantiates a Movie object
-    #    so, when toy_story = media.Movie() in entertainment_center.py then self is toy_story
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    # Remember, init creates space in memory for a new instance of Movie when
+    #    the class is called
+    # Below is also known as the constructor
+    # self is the object/instance being created via the function that
+    #    instantiates a Movie object. So, when toy_story = media.Movie() in
+    # entertainment_center.py then self is toy_story
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         # instance variables
         self.title = movie_title
         self.storyline = movie_storyline
